6340341a3e98a8f2462915fbae2fe31581cfd1e7
Mostafa Ghareeb <mghareeb@sumerge.com>
2023-10-30 07:52:06 +0200

    final removal

--- src/ShareTrading.java
+++ src/ShareTrading.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ShareTrading {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final int N = scanner.nextInt();
-        final int c1 = scanner.nextInt();
-        final int c2 = scanner.nextInt();
+        int N = scanner.nextInt();
+        int c1 = scanner.nextInt();
+        int c2 = scanner.nextInt();
 
 
         int[] sharePrices = startValidations(scanner,N,c1,c2);
